f12e1d83d87a97216ff57c4b3c9cc0fbb66b14ff
IDGS-801-22001372 <81512@alumnos.utleon.edu.mx>
2025-03-03 19:04:36 -0600

    detalles

--- app.py
+++ app.py
@@ -16,10 +16,23 @@ app.config.from_object(DevelopmentConfig)
 csrf = CSRFProtect(app)
 
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    create_form=forms.UserForm2(request.form)
+    alumno=Alumnos.query.all()
+    return render_template("index.html",form=create_form, alumnos=alumno)
+
+@app.route("/detalles", methods=['GET', 'POST'])
+def detalles():
+    if request.method=='GET':
+        id=request.args.get('id')
+        alumno=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        id=request.args.get('id')
+        nombre=alumno.nombre
+        apaterno=alumno.apaterno
+        email=alumno.email
+    return render_template("detalles.html", id=id, nombre=nombre, apaterno=apaterno, email=email)
 
 if __name__ == '__main__':
     csrf.init_app(app)
